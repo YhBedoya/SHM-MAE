75d95ddc35c1eb4a23fddcba0dedfee323f92ec1
YhBedoya <yh.bv.0829@gmail.com>
2023-03-16 21:37:27 +0100

    Burrello2021

--- ReplicaBurrello2021/Burrello2021MethodsReplica.py
+++ ReplicaBurrello2021/Burrello2021MethodsReplica.py
@@ -29,13 +29,13 @@ class SHMDataset(Dataset):
             self.start_time, self.end_time = "05/12/2021 00:00", "05/12/2021 23:59"
             self.datasetSize = 500000
         elif isFineTuning:
-            self.start_time, self.end_time = "06/12/2021 00:00", "06/12/2021 11:59"
+            self.start_time, self.end_time = "06/12/2021 00:00", "06/12/2021 05:59"
             self.datasetSize = 200000
         elif isEvaluation:
-            self.start_time, self.end_time = "06/12/2021 12:00", "06/12/2021 17:59"
+            self.start_time, self.end_time = "06/12/2021 12:00", "06/12/2021 14:59"
             self.datasetSize = 50000
         else:
-            self.start_time, self.end_time = "06/12/2021 17:59", "06/12/2021 23:59"
+            self.start_time, self.end_time = "06/12/2021 18:00", "06/12/2021 20:59"
             self.datasetSize = 50000
         self.path = data_path #'/home/yhbedoya/Repositories/SHM-MAE/traffic/20211205/'
         self.noisySensors = ["C12.1.4", "C17.1.2"]
